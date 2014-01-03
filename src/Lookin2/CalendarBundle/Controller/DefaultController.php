35dbedc3f8ed0569fe21b06000b5be147d3c2621
frian <a@frian.org>
2014-01-03 11:22:26 +0100

    added compass to project

--- src/Lookin2/CalendarBundle/Controller/DefaultController.php
+++ src/Lookin2/CalendarBundle/Controller/DefaultController.php
@@ -7,13 +7,14 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
-{
-    /**
-     * @Route("/hello/{name}")
-     * @Template()
-     */
-    public function indexAction($name)
-    {
-        return array('name' => $name);
-    }
+{	
+	/**
+	 * @Route("/")
+	 * @Template()
+	 */
+	public function indexAction()
+	{
+		return array('msg' => 'app root');
+	}
+
 }
