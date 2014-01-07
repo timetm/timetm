9c526db33a5aaeb678500b7a87d3802973a517b6
frian <a@frian.org>
2014-01-07 15:12:02 +0100

    read only monthly calendar with navigation

--- src/Lookin2/CalendarBundle/Controller/DefaultController.php
+++ src/Lookin2/CalendarBundle/Controller/DefaultController.php
@@ -1,10 +1,14 @@
 <?php
 
+// src/Lookin2/CalendarBundle/Controller/DefaultController.php
+
 namespace Lookin2\CalendarBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Lookin2\CalendarBundle\Helpers\Calendar;
 
 class DefaultController extends Controller
 {	
@@ -17,4 +21,72 @@ class DefaultController extends Controller
 		return array('msg' => 'app root');
 	}
 
+	/**
+	 * @Route("/month/{year}/{month}", name="month")
+	 * @Route("/month")
+	 * @Route("/month/")
+	 * @Template()
+	 */
+	public function monthAction($year = null, $month = null)
+	{
+		
+		$calendar = $this->get('lookin2.calendar');
+		$calendar->setYear($year);
+		$calendar->setMonth($month);
+
+		// -- build monthly navigation links --------------------------------------
+		$PrevYearUrl  = $calendar->getPrevYearUrl();
+		$PrevMonthUrl = $calendar->getPrevMonthUrl();
+		$NextYearUrl  = $calendar->getNextYearUrl();
+		$NextMonthUrl = $calendar->getNextMonthUrl();
+		
+		// -- get month dates -----------------------------------------------------
+		$monthDates = $calendar->getMonthCalendarDates();
+		
+		// -- fill template -------------------------------------------------------
+		return array(
+			'days'         => $monthDates, 
+			'PrevMonthUrl' => $PrevMonthUrl,
+			'NextMonthUrl' => $NextMonthUrl,
+			'PrevYearUrl'  => $PrevYearUrl,
+			'NextYearUrl'  => $NextYearUrl,
+			'CurrentMonth' => $calendar->getCurrentMonth(),
+		);
+	}
+
+	
+	/**
+	 * @Route("/day/{year}/{month}/{day}", name="day")
+	 * @Route("/day")
+	 * @Route("/day/")
+	 * @Template("Lookin2CalendarBundle:Default:index.html.twig")
+	 */
+	public function dayAction($year = null, $month = null, $day = null)
+	{
+		return array('msg' => 'app root');
+// 		$calendar = $this->get('lookin2.calendar');
+// 		$calendar->setYear($year);
+// 		$calendar->setMonth($month);
+	
+// 		// -- build monthly navigation links --------------------------------------
+// 		$PrevYearUrl  = $calendar->getPrevYearUrl();
+// 		$PrevMonthUrl = $calendar->getPrevMonthUrl();
+// 		$NextYearUrl  = $calendar->getNextYearUrl();
+// 		$NextMonthUrl = $calendar->getNextMonthUrl();
+	
+// 		// -- get month dates -----------------------------------------------------
+// 		$monthDates = $calendar->getMonthCalendarDates();
+	
+// 		// -- fill template -------------------------------------------------------
+// 		return array(
+// 				'days'         => $monthDates,
+// 				'PrevMonthUrl' => $PrevMonthUrl,
+// 				'NextMonthUrl' => $NextMonthUrl,
+// 				'PrevYearUrl'  => $PrevYearUrl,
+// 				'NextYearUrl'  => $NextYearUrl,
+// 				'CurrentMonth' => $calendar->getCurrentMonth(),
+// 		);
+	}
+	
+	
 }
---
+++ src/Lookin2/CalendarBundle/Helpers/Calendar.php
@@ -0,0 +1,181 @@
+<?php
+
+// src/Lookin2/CalendarBundle/Helpers/Calendar.php
+
+/**
+ * Class to generate month, week and day views
+ *
+ * @author andre@at-info.ch
+ * @www.at-info.ch
+ */
+namespace Lookin2\CalendarBundle\Helpers;
+
+use Symfony\Component\Routing\Router;
+// use Symfony\Component\Validator as Validator;
+// use Symfony\Component\Validator\Constraints as Assert;
+// use Symfony\Component\Validator\Constraints\Email;
+
+class Calendar {
+
+	/**
+	 * @var integer $year
+	 * 
+	 * @Assert\Type(type="integer") and Assert\Min(0)
+	 *
+	 */
+	private $year;
+	
+	/**
+	 * @var integer $month
+	 *
+	 */
+	private $month;
+	
+	
+	// inject the router
+	protected $router;
+	
+	public function __construct(Router $router) {
+		$this->router = $router;
+	}
+	
+	// set Year
+	public function setYear($year) {
+
+		if (!$year) { $year  = date('Y'); }
+		$this->Year = $year;
+	}
+
+	// get Year
+	public function getYear() {
+		return $this->Year;
+	}
+
+	// set Month
+	public function setMonth($month) {
+		if ( ! $month or $month < 1 or $month > 12 ) { $month = date('m'); }
+		$this->Month = $month;
+		$this->setPrevMonthMonth();
+		$this->setNextMonthMonth();
+		$this->setPrevMonthYear();
+		$this->setNextMonthYear();
+	}
+
+	// get Month
+	// 	public function getMonth() {
+	// 		return $this->Month;
+	// 	}
+
+	// get current month
+	public function getCurrentMonth() {
+		return (int) $this->Month . ' ' . $this->Year;;
+	}
+
+	
+	
+	
+	
+	
+	public function getPrevYearUrl() {
+		$url = $this->router->generate('month', array(
+				'year'  => $this->Year - 1 ,
+				'month' => $this->Month )
+		);
+		return $url;
+	}
+
+	public function setPrevMonthMonth() {
+		$this->PrevMonthMonth = date('m', mktime(0, 0, 0, $this->Month - 1, 1, $this->Year));
+	}
+	
+	public function setPrevMonthYear() {
+		$this->PrevMonthYear = date('Y', mktime(0, 0, 0, $this->Month - 1, 1, $this->Year));
+	}
+	
+	public function setNextMonthMonth() {
+		$this->NextMonthMonth = date('m', mktime(0, 0, 0, $this->Month + 1, 1, $this->Year));
+	}
+	
+	public function setNextMonthYear() {
+		$this->NextMonthYear = date('Y', mktime(0, 0, 0, $this->Month + 1, 1, $this->Year));
+	}
+	
+	
+	public function getPrevMonthUrl() {
+		$url = $this->router->generate('month', array(
+  		'year'  => $this->PrevMonthYear ,
+			'month' => $this->PrevMonthMonth
+		));
+		return $url;
+	}
+
+	public function getNextYearUrl() {
+		$url = $this->router->generate('month', array(
+				'year'  => $this->Year + 1 ,
+				'month' => $this->Month )
+		);
+		return $url;
+	}
+	
+	public function getNextMonthUrl() {
+		$url = $this->router->generate('month', array(
+				'year'  => $this->NextMonthYear ,
+				'month' => $this->NextMonthMonth
+		));
+		return $url;
+	}
+	
+
+	/**
+	 * get the dates to display for a monthly view
+	 */
+	public function getMonthCalendarDates() {
+	
+		$monthDates = array();
+	
+		$currentDayOfWeek = date('N', mktime(0, 0, 0, $this->Month,     1, $this->Year)) - 1;
+		$daysInMonth =      date('t', mktime(0, 0, 0, $this->Month,     1, $this->Year));
+		$daysInLastMonth =  date('t', mktime(0, 0, 0, $this->Month - 1, 1, $this->Year));
+	
+		// -- PREVIOUS MONTH --------------------------------------------------------
+		for($x = 0; $x < $currentDayOfWeek; $x++) {
+			$dayNum = (($daysInLastMonth - ($currentDayOfWeek - 1)) + $x);
+			array_push($monthDates, array ( 
+				'day'     => $dayNum,
+				'dayLink' => $dayNum,
+				'month'   => $this->PrevMonthMonth, 
+				'year'    => $this->PrevMonthYear 
+			));
+		};
+	
+		// -- CURRENT MONTH ---------------------------------------------------------
+		for($dayNum = 1; $dayNum <= $daysInMonth; $dayNum++) {
+			$dayLink = ( $dayNum > 9 ) ? $dayNum : '0'.$dayNum;
+			array_push($monthDates, array ( 
+				'day'     => $dayNum, 
+				'dayLink' => $dayLink, 
+				'month'   => $this->Month, 
+				'year'    => $this->Year 
+			));
+			$currentDayOfWeek++;
+			if($currentDayOfWeek == 7) {
+				$currentDayOfWeek = 0;
+			};
+		};
+	
+		// -- NEXT MONTH ------------------------------------------------------------
+		if ($currentDayOfWeek < 7 && $currentDayOfWeek != 0) {
+			for ($dayNum = 1; $dayNum <= (7 - $currentDayOfWeek); $dayNum++) {
+				$dayLink = ( $dayNum < 10 ) ? '0'.$dayNum : $dayNum;
+				array_push($monthDates, array ( 
+					'day'     => $dayNum, 
+					'dayLink' => $dayLink,
+					'month'   => $this->NextMonthMonth, 
+					'year'    => $this->NextMonthYear 
+				));
+			};
+		};
+	
+		return $monthDates;
+	}
+}
