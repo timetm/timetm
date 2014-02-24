5dcef3f38d04265928e378666ea1d14695c7fe56
frian <a@frian.org>
2014-02-24 11:13:55 +0100

    unified tabs formatting

--- src/Lookin2/CalendarBundle/Model/CalendarDay.php
+++ src/Lookin2/CalendarBundle/Model/CalendarDay.php
@@ -22,220 +22,278 @@ use Symfony\Component\Translation\Translator;
  * class representing a daily calendar
  */
 class CalendarDay extends Calendar {
+  
+  /**
+   * the router service
+   *
+   * @var     \Symfony\Component\Routing\Router
+   */
+  protected $router;
+  
+  /**
+   * the translator service
+   *
+   * @var     \Symfony\Component\Translation\Translator
+   */
+  protected $translator;
+  
+  /**
+   * options
+   *
+   * @var     array
+   */
+  private $options;
+  
+  /**
+   * prevMonthDay
+   *
+   * @var     string
+   */
+  private $prevMonthDay;
+  
+  /**
+   * nextMonthDay
+   *
+   * @var     string
+   */
+  private $nextMonthDay;
 
-	/**
-	 * start hour of the day
-	 * 
-	 * @var     integer
-	 */
-	private $dayStart;
+  /**
+   * start hour of the day
+   * 
+   * @var     integer
+   */
+  private $dayStart;
 
-	/**
-	 * end hour of the day
-	 * 
-	 * @var     integer
-	 */
-	private $dayEnd;
-
-	/**
-	 * Constructor.
-	 *
-	 * @param   service   $router        The router service
-	 * @param   service   $translator    The translation service
-	 * @param   integer   $dayStart      Configuration parameter
-	 * @param   integer   $dayEnd        Configuration parameter
-	 */
-	public function __construct(Router $router, Translator $translator, $dayStart, $dayEnd) {
-		parent::__construct($router, $translator);
-		$this->type = "day";
-		// TODO : parameters validation
-		$this->dayStart = $dayStart;
-		$this->dayEnd   = $dayEnd;
-	}
+  /**
+   * end hour of the day
+   * 
+   * @var     integer
+   */
+  private $dayEnd;
 
 
+  /**
+   * Constructor.
+   *
+   * @param   service   $router        The router service
+   * @param   service   $translator    The translation service
+   * @param   integer   $dayStart      Configuration parameter
+   * @param   integer   $dayEnd        Configuration parameter
+   */
+  public function __construct(Router $router, Translator $translator, $dayStart, $dayEnd) {
+    parent::__construct($router, $translator);
+    $this->type = "day";
+    // TODO : parameters validation
+    $this->dayStart = $dayStart;
+    $this->dayEnd   = $dayEnd;
+  }
 
-	/**
-	 * Set day
-	 *
-	 * @param   string    $day
-	 */
-	private function setDay($day) {
-		// TODO : validation : check if integer, if in month
-		if (!$day) {
-			$day  = date('d');
-		}
-		$this->day = $day;
-	}
 
-	/**
-	 * Set dayName 
-	 */
-	private function setDayName() {
-		$this->dayName = date('D', mktime(0, 0, 0, $this->month, $this->day, $this->year));;
-	}
+  /**
+   * Set day
+   *
+   * @param   string    $day
+   */
+  private function setDay($day) {
+    // TODO : validation : check if integer, if in month
+    if (!$day) {
+      $day  = date('d');
+    }
+    $this->day = $day;
+  }
 
-	/**
-	 * Set prevMonthDay
-	 */
-	private function setPrevMonthDay() {
 
-		$daysInLastMonth =  date('t', mktime(0, 0, 0, $this->month - 1, 1, $this->year));
-		if ( $this->day > $daysInLastMonth ) {
-			$this->prevMonthDay = $daysInLastMonth;
-		}
-		else {
-			$this->prevMonthDay = $this->day;
-		}
-	}
+  /**
+   * Set dayName 
+   */
+  private function setDayName() {
+    $this->dayName = date('D', mktime(0, 0, 0, $this->month, $this->day, $this->year));;
+  }
 
-	/**
-	 * Set additionnal panel navigation parameters.
-	 * 
-	 * set :
-	 * 
-	 * - month
-	 * - monthName
-	 * - day
-	 * - dayName
-	 * - prevMonthDay
-	 * - NextMonthDay
-	 * 
-	 * extends Calender::init
-	 * 
-	 * @see Calender::init()        The extended function
-	 *
-	 * @param   array     $options
-	 */
-	public function ChildInit(array $options = array()) {
-		// set common vars
-		$this->setYear($options['year']);
-		$this->setMonth($options['month']);
-		$this->setDay($options['day']);
-// 		$this->setMonthName();
-		$this->setDayName();
-		$this->setPrevMonthDay();
-		$this->setNextMonthDay();
-	}
+  /**
+   * Set prevMonthDay
+   */
+  private function setPrevMonthDay() {
+  
+    $daysInLastMonth =  date('t', mktime(0, 0, 0, $this->month - 1, 1, $this->year));
+    if ( $this->day > $daysInLastMonth ) {
+      $this->prevMonthDay = $daysInLastMonth;
+    }
+    else {
+      $this->prevMonthDay = $this->day;
+    }
+  }
 
-	
-	/**
-	 * Set additionnal panel navigation parameters.
-	 *
-	 * add the following properties
-	 * 
-	 *  - yesterdayDay.
-	 *  - yesterdayMonth.
-	 *  - yesterdayYear
-	 *  - tomorrowDay.
-	 *  - tomorrowMonth.
-	 *  - tomorrowYear
-	 */
-	public function setAdditionnalNavigationParameters() {
-		$this->yesterdayDay   = date('d', mktime(0, 0, 0, $this->month, $this->day - 1, $this->year));
-		$this->yesterdayMonth = date('m', mktime(0, 0, 0, $this->month, $this->day - 1, $this->year));
-		$this->yesterdayYear  = date('Y', mktime(0, 0, 0, $this->month, $this->day - 1, $this->year));
-		$this->tomorrowDay    = date('d', mktime(0, 0, 0, $this->month, $this->day + 1, $this->year));
-		$this->tomorrowMonth  = date('m', mktime(0, 0, 0, $this->month, $this->day + 1, $this->year));
-		$this->tomorrowYear   = date('Y', mktime(0, 0, 0, $this->month, $this->day + 1, $this->year));
-	}
-	
-	/**
-	 * Set NextMonthDay
-	 */
-	private function setNextMonthDay() {
-	
-		$daysInNextMonth =  date('t', mktime(0, 0, 0, $this->month + 1, 1, $this->year));
-	
-		if ( $this->day > $daysInNextMonth ) {
-			$this->nextMonthDay = $daysInNextMonth;
-		}
-		else {
-			$this->nextMonthDay = $this->day;
-		}
-	}
+  /**
+   * Set additionnal panel navigation parameters.
+   * 
+   * set :
+   * 
+   * - month
+   * - monthName
+   * - day
+   * - dayName
+   * - prevMonthDay
+   * - NextMonthDay
+   * 
+   * extends Calender::init
+   * 
+   * @see Calender::init()        The extended function
+   *
+   * @param   array     $options
+   */
+  public function ChildInit(array $options = array()) {
+    // set common vars
+    $this->setYear($options['year']);
+    $this->setMonth($options['month']);
+    $this->setDay($options['day']);
+    $this->setMonthName();
+    $this->setDayName();
+    $this->setPrevMonthDay();
+    $this->setNextMonthDay();
+  }
 
-	
-	/**
-	 * Get YesterdayUrl
-	 *
-	 * @return string
-	 */
-	public function getYesterdayUrl() {
-		$url = $this->router->generate('day', array(
-			'year'  => $this->yesterdayYear,
-			'month' => $this->yesterdayMonth ,
-			'day'   => $this->yesterdayDay
-		));
-		return $url;
-	}
 
-	
-	/**
-	 * Get YesterdayUrl
-	 *
-	 * @return string
-	 */
-	public function getTomorrowUrl() {
-		$url = $this->router->generate('day', array(
-				'year'  => $this->tomorrowYear,
-				'month' => $this->tomorrowMonth ,
-				'day'   => $this->tomorrowDay
-		));
-		return $url;
-	}
+  /**
+   * Set additionnal panel navigation parameters.
+   *
+   * add the following properties
+   * 
+   *  - yesterdayDay.
+   *  - yesterdayMonth.
+   *  - yesterdayYear
+   *  - tomorrowDay.
+   *  - tomorrowMonth.
+   *  - tomorrowYear
+   */
+  public function setAdditionnalNavigationParameters() {
+    $this->yesterdayDay   = date('d', mktime(0, 0, 0, $this->month, $this->day - 1, $this->year));
+    $this->yesterdayMonth = date('m', mktime(0, 0, 0, $this->month, $this->day - 1, $this->year));
+    $this->yesterdayYear  = date('Y', mktime(0, 0, 0, $this->month, $this->day - 1, $this->year));
+    $this->tomorrowDay    = date('d', mktime(0, 0, 0, $this->month, $this->day + 1, $this->year));
+    $this->tomorrowMonth  = date('m', mktime(0, 0, 0, $this->month, $this->day + 1, $this->year));
+    $this->tomorrowYear   = date('Y', mktime(0, 0, 0, $this->month, $this->day + 1, $this->year));
+  }
 	
 
-	/**
-	 * Get day 
-	 *
-	 * @return string
-	 */
-	public function getDay() {
-		return $this->day;
-	}
+  /**
+   * Set NextMonthDay
+   */
+  private function setNextMonthDay() {
+  
+    $daysInNextMonth =  date('t', mktime(0, 0, 0, $this->month + 1, 1, $this->year));
+    
+    if ( $this->day > $daysInNextMonth ) {
+      $this->nextMonthDay = $daysInNextMonth;
+    }
+    else {
+      $this->nextMonthDay = $this->day;
+    }
+  }
 
-	/**
-	 * Get dayName
-	 *
-	 * @return string
-	 */
-	public function getDayName() {
-		return $this->dayName;
-	}
-
-	/**
-	 * Get day stamp
-	 *
-	 * @return string
-	 */
-	public function getCurrentDayStamp() {
-		$translatedMonthName = $this->translator->trans($this->monthName);
-		$translatedDayName = $this->translator->trans($this->dayName);
-		return $translatedDayName . ', ' . (int)$this->day . ' ' . $translatedMonthName . ' ' . $this->year;
-	}
-
-
-	/**
-	 * get the hours to display for a day view
-	 * 
-	 * @return array $dayTimes    A list of day times by step
-	 */
-	public function getDayTimes() {
 	
-		$step = 60;
+  /**
+   * Get YesterdayUrl
+   *
+   * @return string
+   */
+  public function getYesterdayUrl() {
+    $url = $this->router->generate('day', array(
+      'year'  => $this->yesterdayYear,
+      'month' => $this->yesterdayMonth ,
+      'day'   => $this->yesterdayDay
+    ));
+    return $url;
+  }
 
-		$dayTimes = array();
+
+  /**
+   * Get YesterdayUrl
+   *
+   * @return string
+   */
+  public function getTomorrowUrl() {
+    $url = $this->router->generate('day', array(
+      'year'  => $this->tomorrowYear,
+      'month' => $this->tomorrowMonth ,
+      'day'   => $this->tomorrowDay
+    ));
+    return $url;
+  }
+
+
+  /**
+   * Get day 
+   *
+   * @return string
+   */
+  public function getDay() {
+    return $this->day;
+  }
+
+  /**
+   * Get dayName
+   *
+   * @return string
+   */
+  public function getDayName() {
+    return $this->dayName;
+  }
+
+
+  /**
+   * Get prevMonthDay
+   *
+   * @return string
+   */
+  public function getPrevMonthDay() {
+    return $this->prevMonthDay;
+  }
+
 	
-		for ( $hour = $this->dayStart; $hour <= $this->dayEnd; $hour++ ) {
-			for ( $minsStep = 0; $minsStep < 60; $minsStep += $step ) {
-				$minsStep = ( $minsStep < 10 ) ? '0'.$minsStep : $minsStep;
-				$time = $hour . 'h' . $minsStep;
-				array_push($dayTimes, $time);
-			}
-		}
-	
-		return $dayTimes;
-	}
+  /**
+   * Get nextMonthDay
+   *
+   * @return string
+   */
+  public function getNextMonthDay() {
+    return $this->nextMonthDay;
+  }
+
+
+  /**
+   * Get day stamp
+   *
+   * @return string
+   */
+  public function getCurrentDayStamp() {
+    $translatedMonthName = $this->translator->trans($this->getMonthName());
+    $translatedDayName = $this->translator->trans($this->dayName);
+    return $translatedDayName . ', ' . (int)$this->day . ' ' . $translatedMonthName . ' ' . $this->year;
+  }
+
+
+  /**
+   * get the hours to display for a day view
+   * 
+   * @return array $dayTimes    A list of day times by step
+   */
+  public function getDayTimes() {
+  
+    $step = 60;
+    
+    $dayTimes = array();
+    
+    for ( $hour = $this->dayStart; $hour <= $this->dayEnd; $hour++ ) {
+      for ( $minsStep = 0; $minsStep < 60; $minsStep += $step ) {
+        $minsStep = ( $minsStep < 10 ) ? '0'.$minsStep : $minsStep;
+        $time = $hour . 'h' . $minsStep;
+        array_push($dayTimes, $time);
+      }
+    }
+    
+    return $dayTimes;
+  }
 	
 }
--- src/Lookin2/CalendarBundle/Model/CalendarWeek.php
+++ src/Lookin2/CalendarBundle/Model/CalendarWeek.php
@@ -17,252 +17,246 @@ use Symfony\Component\Translation\Translator;
  */
 class CalendarWeek extends Calendar {
 
-	/**
-	 * the router service
-	 *
-	 * @var     \Symfony\Component\Routing\Router
-	 */
-	protected $router;
+  /**
+   * the router service
+   *
+   * @var     \Symfony\Component\Routing\Router
+   */
+  protected $router;
 
-	/**
-	 * the translator service
-	 *
-	 * @var     \Symfony\Component\Translation\Translator
-	 */
-	protected $translator;
+  /**
+   * the translator service
+   *
+   * @var     \Symfony\Component\Translation\Translator
+   */
+  protected $translator;
 
-	/**
-	 * the week number
-	 *
-	 * @var     string
-	 */
-	protected $weekno;
+  /**
+   * the week number
+   *
+   * @var     string
+   */
+  private $weekno;
 
-	/**
-	 * month number
-	 *
-	 * @var     string
-	 */
-	protected $month;
+  /**
+   * month number
+   *
+   * @var     string
+   */
+  protected $month;
 	
-	/**
-	 * format 
-	 *
-	 * @var     string
-	 */
-	private $format;
+  /**
+   * format 
+   *
+   * @var     string
+   */
+  private $format;
 
-	/**
-	 * options
-	 *
-	 * @var     array
-	 */
-	private $options;
+  /**
+   * options
+   *
+   * @var     array
+   */
+  private $options;
 	
-	/**
-	 * Constructor.
-	 *
-	 * @param   service   $router        The router service
-	 * @param   service   $translator    The translation service
-	 */
-	public function __construct(Router $router, Translator $translator) {
-		parent::__construct($router, $translator);
-	}
+  /**
+   * Constructor.
+   *
+   * @param   service   $router        The router service
+   * @param   service   $translator    The translation service
+   */
+  public function __construct(Router $router, Translator $translator) {
+    parent::__construct($router, $translator);
+  }
 
-	/**
-	 * Set weekno
-	 *
-	 * @param   string    $weekno
-	 */
-	private function setWeekno($weekno) {
-		// TODO : validation : check if integer, if in month
-		if (!$weekno) {
-			$weekno  = date('W');
-		}
-		$this->weekno = $weekno;
-	}
-
-
-	/**
-	 * Get weekno
-	 *
-	 * @return  string
-	 */
-	public function getWeekno() {
-		return $this->weekno;
-	}
-
-	
-	/**
-	 * Set month
-	 *
-	 */
-	protected function setWeekMonth() {
-	
-		$weekMonthes = array();
-	
-		for ( $i = 1; $i < 8; $i++ ) {
-			array_push($weekMonthes, date('m', strtotime($this->year . '-W' . $this->weekno . '-' . $i )));
-		}
-
-		$buffer = array_count_values($weekMonthes);
-
-		$currentCount = 0;
-		$currentMonth = null;
-		foreach ( $buffer as $month => $count ) {
-			
-			if ( $count > $currentCount ) {
-				$currentCount = $count;
-				$currentMonth = $month;
-			}
-		}
-
-		$this->month = $currentMonth;
-
-	}
+  /**
+   * Set weekno
+   *
+   * @param   string    $weekno
+   */
+  private function setWeekno($weekno) {
+    // TODO : validation : check if integer, if in month
+    if (!$weekno) {
+      $weekno  = date('W');
+    }
+    $this->weekno = $weekno;
+  }
 
 
-	/**
-	 * initialize the calendar.
-	 *
-	 * set :
-	 *
-	 * - month
-	 * - monthName
-	 *
-	 * extends Calender::init
-	 * @see Calender::init()        The extended function
-	 *
-	 * @param   mixed     $param
-	 */
-	public function childInit(array $options = array()) {
-		
-		// set common vars
-		$this->setYear($options['year']);
-		$this->setWeekno($options['weekno']);
-		$this->setWeekMonth();
-		
-	}
+  /**
+   * Get weekno
+   *
+   * @return  string
+   */
+  protected function getWeekno() {
+    return $this->weekno;
+  }
 
 
-	/**
-	 * Set additionnal panel navigation parameters
-	 */
-	public function setAdditionnalNavigationParameters() {
-		// dummy;
-	}
+  /**
+   * Set month
+   *
+   */
+  protected function setWeekMonth() {
+
+    $weekMonthes = array();
+    
+    for ( $i = 1; $i < 8; $i++ ) {
+      array_push($weekMonthes, date('m', strtotime($this->getYear() . '-W' . $this->weekno . '-' . $i )));
+    }
+    
+    $buffer = array_count_values($weekMonthes);
+    
+    $currentCount = 0;
+    $currentMonth = null;
+    foreach ( $buffer as $month => $count ) {
+      if ( $count > $currentCount ) {
+        $currentCount = $count;
+        $currentMonth = $month;
+      }
+    }
+    $this->month = $currentMonth;
+  }
 
 
-	/**
-	 * get the dates to display for a weekly view
-	 *
-	 * @return  array     $weekDates    A list of dates
-	 *
-	 */
-	public function getWeekCalendarDates() {
+  /**
+   * initialize the calendar.
+   *
+   * set :
+   *
+   * - month
+   * - monthName
+   *
+   * extends Calender::init
+   * @see Calender::init()        The extended function
+   *
+   * @param   mixed     $param
+   */
+  public function childInit(array $options = array()) {
+  
+    // set common vars
+    $this->setYear($options['year']);
+    $this->setWeekno($options['weekno']);
+    $this->setWeekMonth();
+  }
 
-		$weekDates = array();
 
-		for ( $i = 1; $i < 8; $i++ ) {
-			array_push($weekDates, date('Y-m-d', strtotime($this->year . '-W' . $this->weekno . '-' . $i )));
-		}
+  /**
+   * Set additionnal panel navigation parameters
+   */
+  public function setAdditionnalNavigationParameters() {
+    // dummy;
+  }
 
-		return $weekDates;
-	}
 
-	
-	/**
-	 * Get MonthNameFromMonthNumber
-	 *
-	 * @return  string
-	 */
-	public function getMonthNameFromMonthNumber($month) {
-		
-		$monthName = date("M", mktime(0, 0, 0, $month));
-		return $this->translator->trans($monthName);
-	}
-	
-	
-	/**
-	 * Get WeekStamp
-	 *
-	 * @return  string    $url
-	 */
-	public function getWeekStamp() {
+  /**
+   * get the dates to display for a weekly view
+   *
+   * @return  array     $weekDates    A list of dates
+   *
+   */
+  public function getWeekCalendarDates() {
+  
+    $weekDates = array();
+    
+    for ( $i = 1; $i < 8; $i++ ) {
+      array_push($weekDates, date('Y-m-d', strtotime($this->getYear() . '-W' . $this->weekno . '-' . $i )));
+    }
+    
+    return $weekDates;
+  }
 
-		// day number
-		$lastDayNumOfWeek  = (int)$this->getLastDateOfWeek('d');
-		
-		// month numbers
-		$firstDayMonthNum  = $this->getFirstDateOfWeek('m');
-		$lastDayMonthNum   = $this->getLastDateOfWeek('m');
-		
-		//years
-		$firstDayYear = (int)$this->getFirstDateOfWeek('Y');
-		$lastDayYear  = (int)$this->getLastDateOfWeek('Y');
-		
-		// month names
-		$firstDayMonthName = $this->getMonthNameFromMonthNumber($firstDayMonthNum);
-		$lastDayMonthName  = $this->getMonthNameFromMonthNumber($lastDayMonthNum);
-		
+
+  /**
+   * Get MonthNameFromMonthNumber
+   *
+   * @return  string
+   */
+  public function getMonthNameFromMonthNumber($month) {
+  	
+    $monthName = date("M", mktime(0, 0, 0, $month));
+    return $this->translator->trans($monthName);
+  }
+
+
+  /**
+   * Get WeekStamp
+   *
+   * @return  string    $url
+   */
+  public function getWeekStamp() {
+  
+    // day number
+    $lastDayNumOfWeek  = (int)$this->getLastDateOfWeek('d');
+    
+    // month numbers
+    $firstDayMonthNum  = $this->getFirstDateOfWeek('m');
+    $lastDayMonthNum   = $this->getLastDateOfWeek('m');
+    
+    //years
+    $firstDayYear = (int)$this->getFirstDateOfWeek('Y');
+    $lastDayYear  = (int)$this->getLastDateOfWeek('Y');
+    
+    // month names
+    $firstDayMonthName = $this->getMonthNameFromMonthNumber($firstDayMonthNum);
+    $lastDayMonthName  = $this->getMonthNameFromMonthNumber($lastDayMonthNum);
+    
     $weekStamp = '';
-		
+    
     $weekStamp .= 
       (int)$this->getWeekno() . ', ' . 
-			(int)$this->getFirstDateOfWeek('d') . ' ';
+      (int)$this->getFirstDateOfWeek('d') . ' ';
     
-		// if the week is in one month
-		if ( $firstDayMonthNum == $lastDayMonthNum ) {
+    // if the week is in one month
+    if ( $firstDayMonthNum == $lastDayMonthNum ) {
+    
+      $weekStamp .= ' - ' .
+        $lastDayNumOfWeek . ' ' .
+        $firstDayMonthName . ' ' .
+        $this->getYear();
+    }
+    // if we are in one year 
+    elseif ( $firstDayYear == $lastDayYear ) {
+    
+      $weekStamp .= 
+        $firstDayMonthName . ' - ' .
+        $lastDayNumOfWeek . ' ' .
+        $lastDayMonthName . ' ' .
+        $this->getYear();
+    }
+    // we span 2 years
+    else {
+      $weekStamp .= 
+        $firstDayMonthName . ' ' .
+        $firstDayYear . ' - ' .
+        $lastDayNumOfWeek . ' ' .
+        $lastDayMonthName . ' ' .
+        $lastDayYear;
+    }
+    return $weekStamp;
+  }
 
-			$weekStamp .= ' - ' .
-  			     $lastDayNumOfWeek . ' ' .
-				     $firstDayMonthName . ' ' .
-			       $this->year;
-		}
-		// if we are in one year 
-		elseif ( $firstDayYear == $lastDayYear ) {
 
-			$weekStamp .= 
-			       $firstDayMonthName . ' - ' .
-			       $lastDayNumOfWeek . ' ' .
-			       $lastDayMonthName . ' ' .
-			       $this->year;
-		}
-		// we span 2 years
-		else {
-			$weekStamp .= 
-	     		   $firstDayMonthName . ' ' .
-	     	     $firstDayYear . ' - ' .
-			       $lastDayNumOfWeek . ' ' .
-			       $lastDayMonthName . ' ' .
-			       $lastDayYear;
-		}
-
-		return $weekStamp;
-	}
+  /**
+   * Get FirstDateOfWeek
+   *
+   * @param   string    $format   PHP date format
+   * @return  string
+   */
+  public function getFirstDateOfWeek($format) {
+    return date($format, strtotime($this->getYear() . '-W' . $this->weekno . '-1' ));
+  }
 
 
-	/**
-	 * Get FirstDateOfWeek
-	 *
-	 * @param   string    $format   PHP date format
-	 * @return  string
-	 */
-	public function getFirstDateOfWeek($format) {
-		
-		return date($format, strtotime($this->year . '-W' . $this->weekno . '-1' ));
-	}
-
-	/**
-	 * Get LastDateOfWeek
-	 *
-	 * @param   string    $format   PHP date format
-	 * @return  string
-	 */
-	public function getLastDateOfWeek($format) {
-	
-		return date($format, strtotime($this->year . '-W' . $this->weekno . '-7' ));
-	}
-	
+  /**
+   * Get LastDateOfWeek
+   *
+   * @param   string    $format   PHP date format
+   * @return  string
+   */
+  public function getLastDateOfWeek($format) {
+    return date($format, strtotime($this->getYear() . '-W' . $this->weekno . '-7' ));
+  }
 
 }
+
