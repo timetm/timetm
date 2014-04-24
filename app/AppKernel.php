c6cee1927f8b0160a2174ae58d46d57aff70aa94
frian <a@frian.org>
2014-04-24 14:41:58 +0200

    added FOSUB and Lookin2UserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Lookin2\CalendarBundle\Lookin2CalendarBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Lookin2\UserBundle\Lookin2UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Lookin2/UserBundle/Lookin2UserBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Lookin2\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Lookin2UserBundle extends Bundle {
+	
+	public function getParent() {
+		
+		return 'FOSUserBundle';
+	}
+}
