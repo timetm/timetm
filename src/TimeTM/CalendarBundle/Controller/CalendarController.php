f579709db0437fad18ff607f6030472f4503d6f7
frian <a@frian.org>
2015-04-30 22:22:23 +0200

    added CalendarHelper

--- src/TimeTM/CalendarBundle/Controller/CalendarController.php
+++ src/TimeTM/CalendarBundle/Controller/CalendarController.php
@@ -60,32 +60,13 @@ class CalendarController extends Controller {
 			'type' => $type 
 		));
 
-		// get date for first and last day of month
-		$firstDayOfMonth = date( 'Y-m-d', mktime( 0, 0, 0, $calendar->getMonth(), 1, $calendar->getYear() ) );
-		$lastDayOfMonth  = date( 'Y-m-d', mktime( 0, 0, 0, $calendar->getMonth(), date( 't', mktime( 0, 0, 0, $calendar->getMonth(), 1, $calendar->getYear() ) ), $calendar->getYear() ) );
+		// -- get month dates -------------------------------------------------
+		$monthDates = $calendar->getMonthCalendarDates();
 
-		// get query builder			
-		$queryBuilder = $this->getDoctrine()->getManager()->createQueryBuilder();
+		// get a helper
+		$helper = $this->get('timetm.calendar.helper');
 
-		/*
-		 * build and execute query
-		 * 
-		 * select title,date 
-		 *   from Event e 
-		 *     join Agenda a on e.agenda_id =a.id 
-		 *     join fos_user u on a.user_id=u.id 
-		 *     where a.id=1; TODO
-		 */
-		$events = $queryBuilder
-			->select('partial e.{id, title, place, startdate , starttime }')
-			->from('TimeTMEventBundle:Event', 'e')
-			->leftjoin('e.agenda', 'a')
-			->leftjoin('a.user', 'u')
-	    	->where('e.startdate BETWEEN :firstDay AND :lastDay')
-		    ->setParameter('firstDay', $firstDayOfMonth)
-		    ->setParameter('lastDay', $lastDayOfMonth)
-		    ->getQuery()
-		    ->execute();
+		$monthDates = $helper->addEventsToCalendar($calendar, $monthDates);
 
 		// Possible futur contextual navigation
 		//
@@ -96,21 +77,6 @@ class CalendarController extends Controller {
 		// ));
 		// return $response;
 		// }
-		
-		// -- get month dates -------------------------------------------------
-		$monthDates = $calendar->getMonthCalendarDates();
-
-		// add events to the monthDates array
-		foreach ( $monthDates as &$date ) {
-			if (isset($date['datestamp'])) {
-				$date['events'] = array();
-				foreach ( $events as $event ) {
-					if ( $event->getStartdate()->format('Y-m-d') == $date['datestamp'] ) {
-						array_push($date['events'], $event);
-					}
-				}
-			}
-		}
 
 		// -- create parameters array
 		$params = array (
