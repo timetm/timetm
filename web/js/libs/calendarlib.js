957ec9f0bf7eb87907ed02e6a8af88b7ac589276
frian <a@frian.org>
2015-07-16 16:28:25 +0200

    fixed small css and js bugs

--- web/js/libs/calendarlib.js
+++ web/js/libs/calendarlib.js
@@ -18,16 +18,31 @@
     // get container height
     var displayHeight = $('#content').height();
 
+    console.log('displayHeight : ' + displayHeight);
+
+    
     // get number of rows
     var rowCount = $(rows).length;
 
+    
+    if ( $(cal).attr('data-month') != undefined ) {
+        rowCount = Math.ceil(rowCount / 16);
+    }
+    
+    console.log('rowCount : ' + rowCount);
+    
+    
         // for weeks divide by 7days
         if ( $(cal).attr('data-week') != undefined ) {
             rowCount = ($(rows).length / 7);
         }
 
+        if ( $(cal).attr('data-month') != undefined ) {
+            return ((displayHeight -  rowCount)   / rowCount -1 ) ;
+        }
+        
         // calculate cell height ( we remove rowCount = 1px border )
-        return (displayHeight - rowCount )  / rowCount;
+        return (displayHeight - rowCount)  / rowCount;
     }
 
 
@@ -97,9 +112,9 @@
             var eventList = $(this).children('div');
             var eventCount = eventList.length;
 
-            if ( eventCount > maxEvents - 1) {
+            if ( eventCount > maxEvents ) {
                 // hide event which have no place
-                $(this).find('div:nth-last-child(-n + ' + ( eventCount - maxEvents) + ')').css('display' , 'none');
+                $(this).find('div:nth-last-child(-n + ' + (eventCount - maxEvents) + ')').css('display' , 'none');
             }
         });
     }
@@ -110,16 +125,20 @@
      */
     $.ttm_sizeCalendar = function() {
 
+        
         // get cell height
         var cellHeight = $.ttm_getCellHeight();
 
+        console.log('cellHeight : ' + cellHeight);
+        
         $.ttm_setCellHeight(cellHeight);
-        if (document.querySelector('.event') !== null) {
+//        if (document.querySelector('.event') !== null) {
             $.ttm_setEventHeight(cellHeight);
-        }
-        else if (document.querySelector('.monthEventWrapper') !== null) {
+//        }
+//        else if (document.querySelector('.monthEventWrapper') !== null) {
+            console.log('run month');
             $.ttm_handleMonthEvents(cellHeight);
-        }
+//        }
     }
 
 }(jQuery));
