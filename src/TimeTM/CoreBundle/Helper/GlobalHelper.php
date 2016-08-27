b60ede01b55422f0d6e5f3fe2105a8112bf14f75
frian <a@frian.org>
2016-08-27 12:17:18 +0200

    added getUserAgendaSwitchForm() to globalHelper

--- src/TimeTM/CoreBundle/Helper/GlobalHelper.php
+++ src/TimeTM/CoreBundle/Helper/GlobalHelper.php
@@ -8,6 +8,8 @@
 
 namespace TimeTM\CoreBundle\Helper;
 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 /**
  * class representing a weekly calendar
  */
@@ -25,13 +27,19 @@ class GlobalHelper {
 	 *
 	 * @param EntityManager $em
 	 */
-    public function __construct(\Doctrine\ORM\EntityManager $em, $securityContext) {
+    public function __construct(\Doctrine\ORM\EntityManager $em, $securityContext, $container, $twig) {
 
  		$this->em = $em;
  		$this->context = $securityContext;
+        $this->container = $container;
+        $this->twig = $twig;
  	}
 
-
+    /**
+     * get user theme
+     *
+     * @return string $theme
+     */
     public function getTheme() {
 
         $user = $this->context->getToken()->getUser();
@@ -45,4 +53,26 @@ class GlobalHelper {
         return $theme;
     }
 
+    /**
+     * get user agenda switch select form
+     *
+     * @return string select form
+     */
+     public function getUserAgendaSwitchForm() {
+
+         $formFactory = $this->container->get('form.factory');
+
+         $form = $formFactory->create()
+             ->add('agenda', ChoiceType::class, array(
+                 'choices'  => array(
+                     'Maybe' => null,
+                     'Yes' => true,
+                     'No' => false,
+                 ),
+             )
+         );
+
+         $params = array( 'form' => $form->createView() );
+         return $this->twig->render( 'TimeTMCoreBundle:Default:calendarSwitch.html.twig', $params );
+     }
 }
