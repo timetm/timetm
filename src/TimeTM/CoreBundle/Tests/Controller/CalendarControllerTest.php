932ff1d082c82b517c436e59f0710bd8c775a5b4
frian <a@frian.org>
2016-08-27 17:47:22 +0200

    added calendar controller tests

--- src/TimeTM/CoreBundle/Tests/Controller/CalendarControllerTest.php
+++ src/TimeTM/CoreBundle/Tests/Controller/CalendarControllerTest.php
@@ -7,21 +7,23 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class CalendarControllerTest extends WebTestCase
 {
 
-	public function setUp()
-	{
+	public function setUp() {
+
 		$this->client = static::createClient(array(), array(
 			'PHP_AUTH_USER' => 'admin',
 			'PHP_AUTH_PW'   => '1234',
 		));
 	}
 
-    public function testMonthNoParams()
-    {
-    	print " testing calendar month with a direct get ... ";
 
-        $monthName = date('F');
-        $currentYear = date('Y');
-        $testString = $monthName . " " . $currentYear;
+    /*
+     *  month
+     */
+    public function testMonthNoParams() {
+
+    	print " testing calendar month with a direct get and no parameters ... ";
+
+        $testString = date('F') . " " . date('Y');
 
         $crawler = $this->client->request('GET', '/month/');
 
@@ -30,13 +32,28 @@ class CalendarControllerTest extends WebTestCase
         $this->assertTrue($crawler->filter("html:contains(\"$testString\")")->count() == 1);
     }
 
-    public function testMonthNoParamsFromMainNav()
-    {
+
+    public function testMonth() {
+
+        $params = date('Y') . '/' . date('m');
+
+    	print " testing calendar month with a direct get and parameters $params ... ";
+
+        $testString = date('F') . " " . date('Y');
+
+        $crawler = $this->client->request('GET', "/month/$params");
+
+        print "done.\n";
+
+        $this->assertTrue($crawler->filter("html:contains(\"$testString\")")->count() == 1);
+    }
+
+
+    public function testMonthNoParamsFromMainNav() {
+
     	print " testing calendar month from main navigation ... ";
 
-    	$monthName = date('F');
-    	$currentYear = date('Y');
-    	$testString = $monthName . " " . $currentYear;
+    	$testString = date('F') . " " . date('Y');
 
     	$crawler = $this->client->request('GET', '/');
 
@@ -48,4 +65,103 @@ class CalendarControllerTest extends WebTestCase
 
     	$this->assertTrue($landing->filter("html:contains(\"$testString\")")->count() == 1);
     }
+
+
+    /*
+     *  week
+     */
+    public function testWeekNoParams() {
+
+    	print " testing calendar week with a direct get and no paramters ... ";
+
+        $testString = 'Week' . " " . date('W');
+
+        $crawler = $this->client->request('GET', '/week/');
+
+        print "done.\n";
+
+        $this->assertTrue($crawler->filter("html:contains(\"$testString\")")->count() == 1);
+    }
+
+    public function testWeek() {
+
+        $params = date('Y') . '/' . date('W');
+
+    	print " testing calendar week with a direct get and parameters $params ... ";
+
+        $testString = 'Week' . " " . date('W');
+
+        $crawler = $this->client->request('GET', "/week/$params");
+
+        print "done.\n";
+
+        $this->assertTrue($crawler->filter("html:contains(\"$testString\")")->count() == 1);
+    }
+
+    public function testWeekNoParamsFromPanelNav() {
+
+        print " testing calendar week from panel navigation ... ";
+
+        $testString = 'Week' . " " . date('W');
+
+        $crawler = $this->client->request('GET', '/');
+
+        $link = $crawler->filter('a:contains("Week")')->eq(0)->link();
+
+        $landing = $this->client->click($link);
+
+        print "done.\n\n";
+
+        $this->assertTrue($landing->filter("html:contains(\"$testString\")")->count() == 1);
+    }
+
+
+    /*
+     *  day
+     */
+    public function testDayNoParams() {
+
+    	print " testing calendar day with a direct get and no parameters ... ";
+
+        $testString = date('D') . ", " . date('j') . " " . date('F') . " " . date('Y');
+
+        $crawler = $this->client->request('GET', '/day/');
+
+        print "done.\n";
+
+        $this->assertTrue($crawler->filter("html:contains(\"$testString\")")->count() == 1);
+    }
+
+    public function testDay() {
+
+        $params = date('Y') . '/' . date('W') . '/' . date('d');
+
+    	print " testing calendar week with a direct get and parameters $params ... ";
+
+        $testString = date('D') . ", " . date('j') . " " . date('F') . " " . date('Y');
+
+        $crawler = $this->client->request('GET', "/day/$params");
+
+        print "done.\n";
+
+        $this->assertTrue($crawler->filter("html:contains(\"$testString\")")->count() == 1);
+    }
+
+    public function testDayNoParamsFromPanelNav() {
+
+        print " testing calendar day from panel navigation ... ";
+
+        $testString = date('D') . ", " . date('j') . " " . date('F') . " " . date('Y');
+
+        $crawler = $this->client->request('GET', '/');
+
+        $link = $crawler->filter('a:contains("Day")')->eq(0)->link();
+
+        $landing = $this->client->click($link);
+
+        print "done.\n\n";
+
+        $this->assertTrue($landing->filter("html:contains(\"$testString\")")->count() == 1);
+    }
+
 }
