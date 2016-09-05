394de88b3f31c4646ab714ba5509e2c6ce4bc1db
frian <a@frian.org>
2016-09-05 12:49:47 +0200

    more tests

--- src/TimeTM/CoreBundle/Tests/Controller/AgendaControllerTest.php
+++ src/TimeTM/CoreBundle/Tests/Controller/AgendaControllerTest.php
@@ -4,8 +4,7 @@ namespace TimeTM\CoreBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class AgendaControllerTest extends WebTestCase
-{
+class AgendaControllerTest extends WebTestCase {
 
 	public function setUp() {
 
@@ -27,6 +26,11 @@ class AgendaControllerTest extends WebTestCase
         print "done.\n";
     }
 
+    /**
+     * GET          /
+     * CLIC         admin
+     * CLIC         edit agandas
+     */
     public function testIndexFromProfile() {
 
         printf("%-75s", " agenda index from profile ... ");
@@ -54,7 +58,7 @@ class AgendaControllerTest extends WebTestCase
 
     	$crawler = $this->client->request('GET', '/agenda/new');
 
-    	$this->assertTrue($crawler->filter('html:contains("Agenda creation")')->count() == 1);
+    	$this->assertTrue($crawler->filter('html:contains("add an agenda")')->count() == 1);
 
         print "done.\n";
     }
@@ -69,9 +73,77 @@ class AgendaControllerTest extends WebTestCase
 
     	$landing = $this->client->click($link);
 
-    	$this->assertTrue($landing->filter('html:contains("Agenda creation")')->count() == 1);
+    	$this->assertTrue($landing->filter('html:contains("add an agenda")')->count() == 1);
 
-    	print "done.\n\n\n";
+    	print "done.\n";
     }
 
+    public function testCreate() {
+
+        printf("%-75s", " agenda create with a direct post ... ");
+
+    	$crawler = $this->client->request('GET', '/agenda/new');
+
+    	$this->assertTrue($crawler->filter('html:contains("add an agenda")')->count() == 1);
+
+        $form = $crawler->selectButton('create')->form();
+
+        $form['timetm_agendabundle_agenda[name]'] = 'test';
+        $form['timetm_agendabundle_agenda[description]'] = 'test agenda';
+
+        $crawler = $this->client->submit($form);
+
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+        $this->client->followRedirect();
+        $this->assertContains(
+            'agenda details',
+            $this->client->getResponse()->getContent()
+        );
+        $this->assertContains(
+            'test agenda',
+            $this->client->getResponse()->getContent()
+        );
+
+    	print "done.\n";
+    }
+
+    public function testEdit() {
+
+        printf("%-75s", " agenda edit with a direct get ... ");
+
+    	$crawler = $this->client->request('GET', '/agenda/2/edit');
+
+    	$this->assertTrue($crawler->filter('html:contains("edit agenda")')->count() == 1);
+
+    	print "done.\n";
+    }
+
+    public function testUpdate() {
+
+        printf("%-75s", " agenda update with a direct post ... ");
+
+        $crawler = $this->client->request('GET', '/agenda/2/edit');
+
+        $this->assertTrue($crawler->filter('html:contains("edit agenda")')->count() == 1);
+
+        $form = $crawler->selectButton('update')->form();
+
+        $form['timetm_agendabundle_agenda[name]'] = 'test';
+        $form['timetm_agendabundle_agenda[description]'] = 'test agenda updated';
+
+        $crawler = $this->client->submit($form);
+
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+        $this->client->followRedirect();
+        $this->assertContains(
+            'agenda details',
+            $this->client->getResponse()->getContent()
+        );
+        $this->assertContains(
+            'test agenda updated',
+            $this->client->getResponse()->getContent()
+        );
+
+        print "done.\n\n\n";
+    }
 }
