92d621c210865261eb9325335e04b76eb2495d43
frian <a@frian.org>
2016-10-28 15:14:53 +0200

    generated task

---
+++ src/TimeTM/CoreBundle/Entity/Task.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace TimeTM\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Task
+ *
+ * @ORM\Table(name="task")
+ * @ORM\Entity(repositoryClass="TimeTM\CoreBundle\Repository\TaskRepository")
+ */
+class Task
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="duedate", type="datetime")
+     */
+    private $duedate;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="user", type="integer", nullable=true)
+     */
+    private $user;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="repetition", type="integer", nullable=true)
+     */
+    private $repetition;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Task
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set duedate
+     *
+     * @param \DateTime $duedate
+     *
+     * @return Task
+     */
+    public function setDuedate($duedate)
+    {
+        $this->duedate = $duedate;
+
+        return $this;
+    }
+
+    /**
+     * Get duedate
+     *
+     * @return \DateTime
+     */
+    public function getDuedate()
+    {
+        return $this->duedate;
+    }
+
+    /**
+     * Set user
+     *
+     * @param integer $user
+     *
+     * @return Task
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return int
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set repetition
+     *
+     * @param integer $repetition
+     *
+     * @return Task
+     */
+    public function setRepetition($repetition)
+    {
+        $this->repetition = $repetition;
+
+        return $this;
+    }
+
+    /**
+     * Get repetition
+     *
+     * @return int
+     */
+    public function getRepetition()
+    {
+        return $this->repetition;
+    }
+}
+
---
+++ src/TimeTM/CoreBundle/Form/TaskType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TimeTM\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('duedate', 'datetime')
+            ->add('user')
+            ->add('repetition')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TimeTM\CoreBundle\Entity\Task'
+        ));
+    }
+}
